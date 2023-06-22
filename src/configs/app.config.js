754b123d6c44bd1d2acc5bcf251bff1b9834b618
leoblack <leonardoblack.net@gmail.com>
2023-06-22 16:07:31 -0300

    add NODE_ENV var to api prefix

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -1,6 +1,10 @@
+let apiPrefix = 'http://127.0.0.1:8000/'
+if (process.env.NODE_ENV === 'production') {
+    apiPrefix = 'https://vcbemdevida-back.herokuapp.com/'
+}
+
 const appConfig = {
-    apiPrefix: 'https://vcbemdevida-back.herokuapp.com/',
-    // apiPrefix: 'http://127.0.0.1:8000/',
+    apiPrefix: apiPrefix,
     authenticatedEntryPath: '/home',
     unAuthenticatedEntryPath: '/sign-in',
     tourPath: '/',
