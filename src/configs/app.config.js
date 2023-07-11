5ae6804611f748013ec14e46109d75f224e5de53
leoblack <leonardoblack.net@gmail.com>
2023-07-11 17:44:58 -0300

    create new component to the funnel

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -2,7 +2,7 @@ let apiPrefix = 'http://127.0.0.1:8000/'
 if (process.env.NODE_ENV === 'production') {
     apiPrefix = 'https://vcbemdevida-back.herokuapp.com/'
 }
-apiPrefix = 'https://vcbemdevida-back.herokuapp.com/'
+// apiPrefix = 'https://vcbemdevida-back.herokuapp.com/'
 const appConfig = {
     apiPrefix: apiPrefix,
     authenticatedEntryPath: '/home',
